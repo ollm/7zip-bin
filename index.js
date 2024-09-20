d9fe87371cb26f561b6b63cdeb55326f359c2a7a
Mike Maietta <mmaietta@outlook.com>
2024-09-20 16:40:06 -0700

    initial commit of 7zz binaries from https://www.7-zip.org/download.html [7-Zip 24.08 (2024-08-11)]

--- index.js
+++ index.js
@@ -4,17 +4,17 @@ const path = require("path")
 
 function getPath() {
   if (process.env.USE_SYSTEM_7ZA === "true") {
-    return "7za"
+    return "7zz"
   }
 
   if (process.platform === "darwin") {
-    return path.join(__dirname, "mac", process.arch, "7za")
+    return path.join(__dirname, "mac", process.arch, "7zz")
   }
   else if (process.platform === "win32") {
-    return path.join(__dirname, "win", process.arch, "7za.exe")
+    return path.join(__dirname, "win", process.arch, "7zr.exe")
   }
   else {
-    return path.join(__dirname, "linux", process.arch, "7za")
+    return path.join(__dirname, "linux", process.arch, "7zz")
   }
 }
 
